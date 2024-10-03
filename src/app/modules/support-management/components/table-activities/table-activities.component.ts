fce71f773701ca5899d12a00872e674ea87606c5
felipeunicauca <vsandres@unicauca.edu.co>
2024-10-02 22:56:59 -0500

    working in tables :hammer:

--- src/app/modules/support-management/components/table-activities/table-activities.component.ts
+++ src/app/modules/support-management/components/table-activities/table-activities.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SearchResponse } from '../../../../core/labor.interfaces';
 
 @Component({
   selector: 'app-table-activities',
@@ -8,5 +9,87 @@ import { Component } from '@angular/core';
   styleUrl: './table-activities.component.css'
 })
 export class TableActivitiesComponent {
-
+  public laborData: SearchResponse = {
+    labor: [
+      {
+        type: "Trabajo docencia",
+        activities: [
+          {
+            activities_name: "Metodología de la investigación",
+            source: [
+              {
+                name_source: "Autoevaluación",
+                state: "Pendiente firma",
+                evaluator: "Pepito Perez"
+              },
+              {
+                name_source: "Fuente 2",
+                state: "Diligenciado",
+                rol_evaluator: "Jefe de departamento",
+                evaluator: "Alejandro Toledo Tovar",
+                evaluated: "Pepito Perez"
+              }
+            ]
+          },
+          {
+            activities_name: "Inteligencia Artificial",
+            source: [
+              {
+                name_source: "Autoevaluación",
+                state: "Pendiente",
+                evaluator: "Pepito Perez"
+              },
+              {
+                name_source: "Fuente 2",
+                state: "Pendiente firma",
+                rol_evaluator: "Jefe de departamento",
+                evaluator: "Alejandro Toledo Tovar",
+                evaluated: "Pepito Perez"
+              }
+            ]
+          },
+          {
+            activities_name: "Gestión de proyecto informáticos",
+            source: [
+              {
+                name_source: "Autoevaluación",
+                state: "Pendiente firma",
+                evaluator: "Pepito Perez"
+              },
+              {
+                name_source: "Fuente 2",
+                state: "Diligenciado",
+                rol_evaluator: "Jefe de departamento",
+                evaluator: "Alejandro Toledo Tovar",
+                evaluated: "Pepito Perez"
+              }
+            ]
+          }
+        ]
+      },
+      {
+        type: "Dirección de trabajo de grado",
+        activities: [
+          {
+            activities_name:
+              "Resolución 8.4.3–90.2 / 288, 04 de noviembre de 22,Consejo de Facultad",
+            source: [
+              {
+                name_source: "Autoevaluación",
+                state: "Pendiente firma",
+                evaluator: "Pepito Perez"
+              },
+              {
+                name_source: "Fuente 2",
+                state: "Diligenciado",
+                rol_evaluator: "Estudiante",
+                evaluator: "Federico Sánchez",
+                evaluated: "Pepito Perez"
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  };
 }
