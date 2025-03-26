15cf47853543b778052d0008986106ab908acbf4
felipeunicauca <vsandres@unicauca.edu.co>
2025-03-25 21:08:24 -0500

    formulario autoevaluación :construction:

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,3 +1,14 @@
 export const environments = {
-    baseUrl: "http://localhost:8080"
-}
+  baseUrl: 'http://localhost:8080',
+
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDraRIAHEQTxallqQvuEJ42NetIXjbXpf4',
+    authDomain: 'sed-fiet-unicauca.firebaseapp.com',
+    projectId: 'sed-fiet-unicauca',
+    storageBucket: 'sed-fiet-unicauca.firebasestorage.app',
+    messagingSenderId: '978006211217',
+    appId: '1:978006211217:web:bdfa8b05b30c4d0972dd30',
+    measurementId: 'G-BTLGTJ0CSH',
+  },
+};
