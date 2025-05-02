49f7748ca829c2c84874ae61744b84aa1ee46b97
felipeunicauca <vsandres@unicauca.edu.co>
2025-05-02 09:55:21 -0500

    Fix tipo de actividad

---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,17 @@
+export const environments = {
+    production: true,
+    baseUrl: 'http://localhost:8080',
+    baseUrlStatistics: 'http://localhost:8086',
+    baseUrlAuth: 'http://localhost:8087',
+    baseUrlLaborDocente: 'http://localhost:8085',
+
+    firebaseConfig: {
+        apiKey: 'AIzaSyDraRIAHEQTxallqQvuEJ42NetIXjbXpf4',
+        authDomain: 'sed-fiet-unicauca.firebaseapp.com',
+        projectId: 'sed-fiet-unicauca',
+        storageBucket: 'sed-fiet-unicauca.firebasestorage.app',
+        messagingSenderId: '978006211217',
+        appId: '1:978006211217:web:bdfa8b05b30c4d0972dd30',
+        measurementId: 'G-BTLGTJ0CSH',
+    },
+};
